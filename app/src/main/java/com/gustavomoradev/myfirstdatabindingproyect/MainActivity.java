451d950a4eb9facada38a540c8ad5f93b719469f
Gustavo Mora <gtmora200@gmail.com>
2022-12-27 13:17:05 -0600

    add databinding

--- app/src/main/java/com/gustavomoradev/myfirstdatabindingproyect/MainActivity.java
+++ app/src/main/java/com/gustavomoradev/myfirstdatabindingproyect/MainActivity.java
@@ -1,14 +1,40 @@
 package com.gustavomoradev.myfirstdatabindingproyect;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.gustavomoradev.myfirstdatabindingproyect.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding =  DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        initActivity();
+    }
+
+    private void initActivity() {
+        setUpView();
+        setUpEvents();
+    }
+
+    private void setUpView() {
+
+    }
+
+    private void setUpEvents() {
+        binding.header.btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
